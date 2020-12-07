3611c412e50bc52da957e9ec624b4cc1b6650a0c
frevo-works-ono <h.ono@frevo-works.co.jp>
2020-12-07 23:08:32 +0900

    bluetooth機能修正
    スキャン結果を画面に表示

--- lib/pages/bluetooth.dart
+++ lib/pages/bluetooth.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:flutter_mobile/common/importer.dart';
@@ -10,6 +12,14 @@ class BluetoothPage extends StatefulWidget {
 }
 
 class _BluetoothPageState extends State<BluetoothPage> {
+  final _streamController = StreamController<List<ScanResult>>();
+
+  @override
+  void dispose() {
+    _streamController.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,16 +29,36 @@ class _BluetoothPageState extends State<BluetoothPage> {
           ElevatedButton(
               onPressed: () {
                 FlutterBlue flutterBlue = FlutterBlue.instance;
+
                 flutterBlue.startScan(timeout: Duration(seconds: 2));
                 var subscription = flutterBlue.scanResults.listen((results) {
                   // do something with scan results
-                  for (ScanResult r in results) {
-                    debugPrint('${r.device.name} が見つかった rssi: ${r.rssi}');
-                  }
+                  this._streamController.sink.add(results);
+                  // for (ScanResult r in results) {
+                  //   debugPrint('${r.device.name} が見つかった rssi: ${r.rssi}');
+                  // }
                 });
                 flutterBlue.stopScan();
               },
-              child: Icon(Icons.ac_unit))
+              child: Text("スキャン開始")),
+          StreamBuilder(
+              stream: this._streamController.stream,
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<ScanResult>> snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      ScanResult scanResult = snapshot.data[index];
+
+                      return Card(
+                          child: ListTile(title: Text(scanResult.device.name)));
+                    },
+                  );
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              })
         ],
       ),
     );
