3ed89aee63cf74644d8f8f99d2bf3f4b9c7d5776
frevo-works-ono <h.ono@frevo-works.co.jp>
2020-12-07 16:53:29 +0900

    Bluetoothサンプル実装

--- lib/pages/bluetooth.dart
+++ lib/pages/bluetooth.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 import 'package:flutter_mobile/common/importer.dart';
 
 class BluetoothPage extends StatefulWidget {
@@ -13,7 +14,23 @@ class _BluetoothPageState extends State<BluetoothPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: HeaderComponent(title: "Bluetooth"),
-      body: Column(),
+      body: Column(
+        children: <Widget>[
+          ElevatedButton(
+              onPressed: () {
+                FlutterBlue flutterBlue = FlutterBlue.instance;
+                flutterBlue.startScan(timeout: Duration(seconds: 2));
+                var subscription = flutterBlue.scanResults.listen((results) {
+                  // do something with scan results
+                  for (ScanResult r in results) {
+                    debugPrint('${r.device.name} が見つかった rssi: ${r.rssi}');
+                  }
+                });
+                flutterBlue.stopScan();
+              },
+              child: Icon(Icons.ac_unit))
+        ],
+      ),
     );
   }
 }
