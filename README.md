c09c936f34e456d03bc2d01ec1fa67a244339fd5
k-saito-frevo <k.saito@frevo-works.co.jp>
2020-12-08 17:01:47 +0900

    bluetooth実行可能に変更

--- README.md
+++ README.md
@@ -17,5 +17,34 @@ samples, guidance on mobile development, and a full API reference.
 
 # 初回起動時はこちらを実行してください
 ①プロジェクト直下で flutter pub get　を実行
+
 ②flutter pub pub run flutter_native_splash:create　を実行
+
 ③ビルドして実行
+
+#　iOSの実機ビルドをする時はこちらの手順に従ってください
+①cd ios でiosフォルダに入る
+②pod install　を実行
+③Xcodeを開く
+④XCodeのPodsプロジェクトを開く
+⑤Quoted Include In Framework Header をNoに変更
+⑥Pods/Pods/flutter_blue.podspecを開き下記２箇所を変更
+```
+Pod::Spec.new do |s|
+  s.name             = 'flutter_blue'
+  s.version          = '0.7.3' #←をこれに変更
+  s.summary          = 'Bluetooth Low Energy plugin for Flutter.'
+  s.description      = <<-DESC
+```
+
+```
+s.subspec "Protos" do |ss|
+    ss.source_files = "gen/*.pbobjc.{h,m}", "gen/**/*.pbobjc.{h,m}"
+    #ss.header_mappings_dir = "gen"
+    ss.requires_arc = false
+    ss.dependency "Protobuf" #←に変更
+  end
+```
+
+⑦ビルドして実行
+
--- lib/pages/bluetooth.dart
+++ lib/pages/bluetooth.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobile/common/importer.dart';
-//import 'package:flutter_blue/flutter_blue.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
 class BluetoothPage extends StatefulWidget {
   BluetoothPage({Key key}) : super(key: key);
@@ -10,10 +10,10 @@ class BluetoothPage extends StatefulWidget {
 }
 
 class _BluetoothPageState extends State<BluetoothPage> {
-  // BluetoothDevice device;
-  // BluetoothState state;
-  // BluetoothDeviceState deviceState;
-  // FlutterBlue flutterBlue = FlutterBlue.instance;
+  BluetoothDevice device;
+  BluetoothState state;
+  BluetoothDeviceState deviceState;
+  FlutterBlue flutterBlue = FlutterBlue.instance;
   var scanSubscription;
   Future<void> checkBlueTooth() async {
     // FlutterBlue flutterBlue = FlutterBlue.instance;
@@ -41,24 +41,24 @@ class _BluetoothPageState extends State<BluetoothPage> {
 // Stop scanning
     //   flutterBlue.stopScan();
   }
-  // void scanForDevices() async {
-  //   scanSubscription = flutterBlue.scan().listen((scanResult) async {
-  //     if (scanResult.device.name == "your_device_name") {
-  //       print("found device");
-  //       //Assigning bluetooth device
-  //       device = scanResult.device;
-  //       //After that we stop the scanning for device
-  //       stopScanning();
-  //     } else {
-  //       print(scanResult.device);
-  //     }
-  //   });
-  // }
+  void scanForDevices() async {
+    scanSubscription = flutterBlue.scan().listen((scanResult) async {
+      if (scanResult.device.name == "your_device_name") {
+        print("found device");
+        //Assigning bluetooth device
+        device = scanResult.device;
+        //After that we stop the scanning for device
+        stopScanning();
+      } else {
+        print(scanResult.device);
+      }
+    });
+  }
 
-  // void stopScanning() {
-  //   flutterBlue.stopScan();
-  //   scanSubscription.cancel();
-  // }
+  void stopScanning() {
+    flutterBlue.stopScan();
+    scanSubscription.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,24 +66,24 @@ class _BluetoothPageState extends State<BluetoothPage> {
       appBar: HeaderComponent(title: "Bluetooth"),
       body: Column(
         children: <Widget>[
-          // ElevatedButton(
-          //     onPressed: () async {
-          //       flutterBlue.startScan(timeout: Duration(seconds: 2));
-          //       var subscription = flutterBlue.scanResults.listen((results) {
-          //         for (ScanResult r in results) {
-          //           debugPrint('${r.device.name} が見つかった rssi: ${r.rssi}');
-          //         }
-          //       });
-          //       flutterBlue.stopScan();
-          //       FlutterBlue.instance.state.listen((state) {
-          //         if (state == BluetoothState.off) {
-          //           //Alert user to turn on bluetooth.
-          //         } else if (state == BluetoothState.on) {
-          //           scanForDevices();
-          //         }
-          //       });
-          //     },
-          //     child: Icon(Icons.ac_unit))
+          ElevatedButton(
+              onPressed: () async {
+                flutterBlue.startScan(timeout: Duration(seconds: 2));
+                var subscription = flutterBlue.scanResults.listen((results) {
+                  for (ScanResult r in results) {
+                    debugPrint('${r.device.name} が見つかった rssi: ${r.rssi}');
+                  }
+                });
+                flutterBlue.stopScan();
+                FlutterBlue.instance.state.listen((state) {
+                  if (state == BluetoothState.off) {
+                    //Alert user to turn on bluetooth.
+                  } else if (state == BluetoothState.on) {
+                    scanForDevices();
+                  }
+                });
+              },
+              child: Icon(Icons.ac_unit))
         ],
       ),
     );
